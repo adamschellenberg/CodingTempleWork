556b5145950c7e4b1410878db379ed03e89fd4e8
Adam Schellenberg <adamschellenberg93@gmail.com>
2022-08-22 13:55:32 -0700

    completed Zen of Python section

--- PythonCrashCourse-Chapter2.py
+++ PythonCrashCourse-Chapter2.py
@@ -1,3 +1,6 @@
+# Python Crash Course, Chapter 2
+# Aug, 22, 2022 
+
 # SECTION: Variables TRY IT YOURSELF, page 23
 # 2-1 Simple Message:
 message = "Hello"
@@ -9,6 +12,7 @@ print (message_1)
 message_1 = message_1 + " World"
 print (message_1)
 
+####################
 
 # SECTION: Strings TRY IT YOURSELF, page 29
 # 2-3 Personal Message
@@ -33,3 +37,16 @@ print (name_2)
 print (name_2.lstrip())
 print (name_2.rstrip())
 print (name_2.strip())
+
+####################
+
+# SECTION: Numbers TRY IT YOURSELF, page 33
+# 2-8 Number Eight
+
+print (4 + 4)
+print (10 - 2)
+print (4 * 2)
+print (int(16 / 2))
+
+# 2-11 Zen of Python
+import this
