d69ffba90debe63c4d7d523fb73d9adcae811143
Adam Schellenberg <adamschellenberg93@gmail.com>
2022-08-22 12:21:16 -0700

    completed Variables section of Python Crash Course, Chapter 2

---
+++ PythonCrashCourse-Chapter2.py
@@ -0,0 +1,10 @@
+# SECTION: Variables TRY IT YOURSELF, page 23
+# 2-1 Simple Message:
+message = "Hello"
+print (message)
+
+# 2-2 Simple Messages:
+message_1 = "Hello"
+print (message_1)
+message_1 = message_1 + " World"
+print (message_1)
