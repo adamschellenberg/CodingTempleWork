2b8ea0e421b44f19b9410f86e87295344b049388
Adam Schellenberg <adamschellenberg93@gmail.com>
2022-08-22 13:33:40 -0700

    Completed String section

--- PythonCrashCourse-Chapter2.py
+++ PythonCrashCourse-Chapter2.py
@@ -8,3 +8,28 @@ message_1 = "Hello"
 print (message_1)
 message_1 = message_1 + " World"
 print (message_1)
+
+
+# SECTION: Strings TRY IT YOURSELF, page 29
+# 2-3 Personal Message
+name = "Adam"
+print ("Hello " + name + ", would you like to play a game?")
+
+# 2-4 Name Cases
+name_1 = "Adam Schellenberg"
+print (name_1.lower())
+print (name_1.upper())
+print (name_1.title())
+
+# 2-5 Famous Quote + 2-6 Famous Quote 2
+quote = "Simplicity is the ultimate sophistication."
+famous_person = "LeOnArDo Da ViNcI"
+quote_message = famous_person.title() + ' once said, "' + quote + '"'
+print (quote_message)
+
+# 2-7 Stripping Names
+name_2 = "    I am\tAdam\n\tSchellenberg. Nice to meet you.   "
+print (name_2)
+print (name_2.lstrip())
+print (name_2.rstrip())
+print (name_2.strip())
