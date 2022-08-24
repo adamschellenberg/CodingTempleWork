ae1ae92989e3af6be7376976e0e7f67de21c8ea6
Adam Schellenberg <adamschellenberg93@gmail.com>
2022-08-24 09:29:23 -0700

    organized folder. started python prework assignment

---
+++ Python101-102/PythonPreworkAssignment.py
@@ -0,0 +1,34 @@
+# QUESTION 1
+# Write a function to print "hello_USERNAME!"
+# USERNAME is the input of the function
+def hello_name(user_name):
+
+
+
+# QUESTION 2
+# Write a python function, first_odds that prints the
+# odd numvers from 1-100 and returns nothing
+# def first_odds():
+
+
+
+# QUESTION 3
+# Please write a Python function, max_num_in_list to return the max
+# number of a given list.
+# def max_num_in_list(a_list):
+
+
+
+# QUESTION 4
+# Write a function to return if the given year is a leap year.
+# A leap year is divisble by 4, but not divisible by 100, unless
+# it is also divisibl by 400. The return should be a bool
+# def is_leap_year(a_year):
+
+
+
+# QUESTION 5
+# Write a function to check to see if all numbers in list are consecutive numbers
+# For example, [2,3,4,5,6,7] are consecutive numbers,
+# but [1,2,4,5] are not. The return should be a bool
+# def is_consecutive(a_list):
